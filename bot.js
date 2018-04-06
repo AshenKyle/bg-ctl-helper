92d39557e0439f992fe1e3761a7e07334c3030b2
AsheN <ashengale01@gmail.com>
2018-04-06 21:19:50 +0200

    * added prefix functionality, change msg func

--- bot.js
+++ bot.js
@@ -6,12 +6,16 @@ client.on("ready", () => {
 });
 
 client.on("message", (message) => {
-  if (message.content === "ping"){
-    message.reply("pong");
-  }
-  if (message.content === "message"){
-    message.reply(message);
-  }
+    if(message.content[0] === process.env.PREFIX){  
+     message.content = message.content.substr(1, message.content.length);
+        if (message.content === "ping"){
+        message.reply("pong");
+        }
+        if (message.content === "message"){
+        message.reply("success");
+        message.reply(message.channel);
+        }
+    }
 });
 
 client.login(process.env.BOT_TOKEN);
