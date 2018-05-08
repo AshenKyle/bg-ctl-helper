31f288570179de7a471184c596ec4a838fea03ba
AsheN <ashengale01@gmail.com>
2018-05-08 15:07:54 +0200

    + added tryout/promotion functions, refactor online/local switching

--- bot.js
+++ bot.js
@@ -1,13 +1,15 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const prefix = "_";
+const online = true;
+const prefix = online ?  process.env.PREFIX : "_";
+const guildName = online ? "Born Gosu Gaming" : "Pantsu";
 let races = [], teamLineup = [], league = [], ctlProfiles = [], enemyIGN = [], teamIGN = [], score = [], topic;
 const sc2unmaskedLink = "http://sc2unmasked.com/Search?q=";
 let channel = "";
 
 client.on("ready", () => {
-     client.user.setUsername("Ashley");
-    channel = client.guilds.find("name","Born Gosu Gaming").channels.find("name", "ctl");
+    client.user.setUsername("Ashley");
+    channel = client.guilds.find("name", guildName).channels.find("name", "ctl");
     client.user.setActivity("CTL Simulator", { type: "PLAYING"});
     for(var i = 0; i < 7; i++) {
         switch (i){
@@ -34,7 +36,7 @@ client.on("message", (message) => {
     if(message.author.username !== client.user.username) {
         var msg = message.content.substr(1, message.content.length);
         var outputStr;
-        if (message.content[0] === process.env.PREFIX) {
+        if (message.content[0] === prefix) {
             if(msg.substr(0, 4) === "help"){
                 manualPage(message.author.username);
                 return;
@@ -46,7 +48,18 @@ client.on("message", (message) => {
                 message.channel.send("Received 1 _AsheN Coin_");
             }
             if(adminCheck(message.author.lastMessage.member.roles.find('name', 'Admins'))){
-                if (msg.substr(0, 6) === "submit") {
+                var command = msg.split(" ");
+                if(command[0] === "tryout"){
+                    if(command[1] !== null || command[2] !== null || command[3] !== null){
+                        tryout(message.channel, message.mentions.users, command[1], command[2], command[3]);
+                    }
+                }
+                else if(command[0] === "promote"){
+                    if(command[1] !== null){
+                        promote(message.channel, message.mentions.users, command[1]);
+                    }
+                }
+                else if (msg.substr(0, 6) === "submit") {
                     ctlLineup(msg.substr(7, msg.length));
                     //done(message.channel);
                     channel.send("Done.");
@@ -114,7 +127,7 @@ client.on("message", (message) => {
                             "\nLink(s):\n" + sc2unmaskedLink + enemyIGN[index] + "\n" + ctlProfiles[index] + "\n\n";
                     });
                     ctlTopic(teamIGN, week);
-                    outputStr += "**GLHF everyone!** "+client.guilds.find("name","Born Gosu Gaming").roles.find("name", "CTL Players");
+                    outputStr += "**GLHF everyone!** "+client.guilds.find("name", guildName).roles.find("name", "CTL Players");
                     channel.send(outputStr)
                         .then(() => channel.fetchMessages({limit:1})
                             .then(messages => {
@@ -193,6 +206,45 @@ function ctlTopic(team, week = "", set = "", str = ""){
     channel.setTopic(topic).then().catch(console.error);
 }
 
+function tryout(channel, user, mentionUser, league, race) {
+    if (league.toLowerCase() === "gm"){
+        league = "Grand Master";
+    }
+
+    var tryoutInfo = "After filling out our recruitment application you have now been given the Tryout Role which represents a trial period in the team. You will continue to have this role for about 1-3 weeks ( depending on your activity ), In that time you can make yourself a part of the community while we review your application! \n" +
+        "\n" +
+        "We adopt the system of trial membership before official membership to filter out trolls / inactive members out of the team, you can expect a fast promotion if you're active in our discord community and participate in clan-wars,pratice games, team leagues etc. If you have any questions regarding the team in general or your membership feel free to let us know ^^ \n"+
+        "\n" + "_(P.S. I'm a bot.)_";
+
+    var leagueString = league[0].toUpperCase() + league.substr(1, league.length);
+    var raceString = race[0].toUpperCase() + race.substr(1, race.length);
+    user = user.array();
+    var tryoutMember = client.users.find("id", user[0].id);
+
+    channel.send("Welcome our newest Tryout to Born Gosu! " + mentionUser + " @here");
+    var roles = client.guilds.find("name", guildName).roles;
+
+    tryoutMember.send(tryoutInfo);
+
+    var guildMember = client.guilds.find("name", guildName).member(tryoutMember);
+    guildMember.addRole(roles.find("name", "Tryout Member").id);
+    guildMember.addRole(roles.find("name", leagueString).id);
+    guildMember.addRole(roles.find("name", raceString).id);
+    guildMember.removeRole(roles.find("name", "Non-Born Gosu").id);
+}
+
+function promote(channel, user, mentionUser){
+    user = user.array();
+    var tryoutMember = client.users.find("id", user[0].id);
+
+    channel.send("Welcome our newest Born Gosu member! " + mentionUser + " @here");
+    var roles = client.guilds.find("name", guildName).roles;
+
+    var guildMember = client.guilds.find("name", guildName).member(tryoutMember);
+    guildMember.addRole(roles.find("name", "Born Gosu").id);
+    guildMember.removeRole(roles.find("name", "Tryout Member").id);
+}
+
 function adminCheck(param) {
     if(param) return true;
     else return false;
@@ -222,7 +274,7 @@ function manualPage(username) {
          */
         .addBlankField(true)
         .addField("submit",
-            "Syntax: "+process.env.PREFIX+"submit _copy paste lineups from ctl page here_ \n" +
+            "Syntax: "+prefix+"submit _copy paste lineups from ctl page here_ \n" +
             "Example: +submit SweatyHeart | SweatyHeart#1380 vs. Hillnor | Hillnor#2890 [Acid Plant LE]\n" +
             "Migwel | Migwel#2942 vs. Desperoth | Desperoth#2896 [Abiogenesis LE]\n" +
             "Warbuffll | Warbuffll#1927 vs. Heisswasser | Gorylov17#2529 [Blackpink LE]\n" +
@@ -235,14 +287,14 @@ function manualPage(username) {
          */
         .addBlankField(true)
         .addBlankField(true)
-        .addField("races", "Syntax: "+process.env.PREFIX+"races _enter races of the players in order of the sets and from left to right here_\n" +
+        .addField("races", "Syntax: "+prefix+"races _enter races of the players in order of the sets and from left to right here_\n" +
             "Example: +races pprzpttztpzzzt")
         /*
          * Blank field, useful to create some space.
          */
         .addBlankField(true)
         .addBlankField(true)
-        .addField("profiles", "Syntax: "+process.env.PREFIX+"profiles _copy paste each of the enemy players ctl profiles here_\n" +
+        .addField("profiles", "Syntax: "+prefix+"profiles _copy paste each of the enemy players ctl profiles here_\n" +
             "Example: +profiles http://www.choboteamleague.com/profile/16638891\n" +
             "http://www.choboteamleague.com/profile/19219318\n" +
             "http://www.choboteamleague.com/profile/10735948\n" +
@@ -252,18 +304,20 @@ function manualPage(username) {
             "http://www.choboteamleague.com/profile/3163662")
         .addBlankField(true)
         .addBlankField(true)
-        .addField("lineups", "Syntax: "+process.env.PREFIX+"lineups _week-number_ 'left/right'\n" +
+        .addField("lineups", "Syntax: "+prefix+"lineups _week-number_ 'left/right'\n" +
             "Example: +lineups 8 left\n" +
             "Example: +lineups 1 right")
         .addBlankField(true)
         .addBlankField(true)
-        .addField("Other Commands", process.env.PREFIX+"update _set-number_ _w/l/status_\n" +
+        .addField("Other Commands", prefix+"update _set-number_ _w/l/status_\n" +
+            prefix+"tryout @user _league_ _race_\n" +
+            prefix+"promote @user\n" +
             "If you need more detailed information please message AsheN!")
         .addBlankField(true)
         .addBlankField(true)
-        .addField("Miscellaneous", process.env.PREFIX+"help\n" +
-            process.env.PREFIX+"ping\n"+
-            process.env.PREFIX+"ashencoins\n");
+        .addField("Miscellaneous", prefix+"help\n" +
+            prefix+"ping\n"+
+            prefix+"ashencoins\n");
     // Intro
     /*
     client.users.find("username", username).send({
