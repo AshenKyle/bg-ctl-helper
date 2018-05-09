8f25ef8e6f9b69c4d7c40c83c569731e78c52235
AsheN <ashengale01@gmail.com>
2018-05-09 10:27:24 +0200

    * refactor code, admin check

--- bot.js
+++ bot.js
@@ -8,10 +8,11 @@ const sc2unmaskedLink = "http://sc2unmasked.com/Search?q=";
 let channel = "";
 
 client.on("ready", () => {
+    client.users.find("username", "AsheN").send("READY FOR ACTON");
     client.user.setUsername("Ashley");
     channel = client.guilds.find("name", guildName).channels.find("name", "ctl");
     client.user.setActivity("CTL Simulator", { type: "PLAYING"});
-    for(var i = 0; i < 7; i++) {
+    for(let i = 0; i < 7; i++) {
         switch (i){
             case 0:
                 league.push(client.emojis.find("name", "Gold"));
@@ -34,8 +35,9 @@ client.on("ready", () => {
 
 client.on("message", (message) => {
     if(message.author.username !== client.user.username) {
-        var msg = message.content.substr(1, message.content.length);
-        var outputStr;
+        let msg = message.content.substr(1, message.content.length);
+        let command = msg.split(" ");
+        let outputStr;
         if (message.content[0] === prefix) {
             if(msg.substr(0, 4) === "help"){
                 manualPage(message.author.username);
@@ -47,8 +49,8 @@ client.on("message", (message) => {
             else if (msg.substr(0, 10) === "ashencoins") {
                 message.channel.send("Received 1 _AsheN Coin_");
             }
+            else if (command[0] === "admin") message.channel.send(adminCheck(message.author.lastMessage.member.roles.find('name', 'Admins')));
             if(adminCheck(message.author.lastMessage.member.roles.find('name', 'Admins'))){
-                var command = msg.split(" ");
                 if(command[0] === "tryout"){
                     if(command[1] !== null || command[2] !== null || command[3] !== null){
                         tryout(message.channel, message.mentions.users, command[1], command[2], command[3]);
@@ -84,9 +86,9 @@ client.on("message", (message) => {
                     channel.send("Done.");
                 }
                 else if (msg.substr(0, 7) === "lineups") {
-                    var week = msg.substr(8, 1);
-                    var side = msg.substr(10, 7);
-                    var teamRaces = [], enemyRaces = [];
+                    let week = msg.substr(8, 1);
+                    let side = msg.substr(10, 7);
+                    let teamRaces = [], enemyRaces = [];
                     if(side !== "left" && side !== "right"){
                         message.channel.send("Please correctly specify the side BornGosu is on! (left/right)");
                         return;
@@ -112,9 +114,9 @@ client.on("message", (message) => {
                     channel.send("Done.");
                     outputStr = "__**CTL Lineups FINALS :**__\n";
                     teamLineup.forEach(function(element, index){
-                        var coreStr = element.substr(0, element.indexOf("["));
-                        var left = coreStr.substr(0, coreStr.indexOf("|"));
-                        var right = coreStr.substr(coreStr.indexOf("vs. ")+4, coreStr.substr(coreStr.indexOf("vs. "), coreStr.length).indexOf("|")-4);
+                        let coreStr = element.substr(0, element.indexOf("["));
+                        let left = coreStr.substr(0, coreStr.indexOf("|"));
+                        let right = coreStr.substr(coreStr.indexOf("vs. ")+4, coreStr.substr(coreStr.indexOf("vs. "), coreStr.length).indexOf("|")-4);
                         if (side) {
                             enemyIGN[index] = left;
                             teamIGN[index] = right;
@@ -146,7 +148,7 @@ client.on("message", (message) => {
 
 function ctlLineup(lineup){
     teamLineup = [];
-    var lineupArr = lineup.split("\n");
+    let lineupArr = lineup.split("\n");
     lineupArr.forEach(function(lineupStr){
         teamLineup.push(lineupStr);
     });
@@ -154,7 +156,7 @@ function ctlLineup(lineup){
 
 function ctlProfile(profiles){
     ctlProfiles = [];
-    var profilesArray = profiles.split("\n");
+    let profilesArray = profiles.split("\n");
     profilesArray.forEach(function(prof){
         ctlProfiles.push(prof);
     });
@@ -162,7 +164,7 @@ function ctlProfile(profiles){
 
 function lineupRaces(message){
     races = [];
-    for(var i=0; i < message.length; i++){
+    for(let i=0; i < message.length; i++){
         switch (message[i].toLowerCase()){
             case "t": races.push(client.emojis.find("name", "Terran")+"");
                 break;
@@ -211,22 +213,22 @@ function tryout(channel, user, mentionUser, league, race) {
         league = "Grand Master";
     }
 
-    var tryoutInfo = "After filling out our recruitment application you have now been given the Tryout Role which represents a trial period in the team. You will continue to have this role for about 1-3 weeks ( depending on your activity ), In that time you can make yourself a part of the community while we review your application! \n" +
+    let tryoutInfo = "After filling out our recruitment application you have now been given the Tryout Role which represents a trial period in the team. You will continue to have this role for about 1-3 weeks ( depending on your activity ), In that time you can make yourself a part of the community while we review your application! \n" +
         "\n" +
         "We adopt the system of trial membership before official membership to filter out trolls / inactive members out of the team, you can expect a fast promotion if you're active in our discord community and participate in clan-wars,pratice games, team leagues etc. If you have any questions regarding the team in general or your membership feel free to let us know ^^ \n"+
         "\n" + "_(P.S. I'm a bot.)_";
 
-    var leagueString = league[0].toUpperCase() + league.substr(1, league.length);
-    var raceString = race[0].toUpperCase() + race.substr(1, race.length);
+    let leagueString = league[0].toUpperCase() + league.substr(1, league.length);
+    let raceString = race[0].toUpperCase() + race.substr(1, race.length);
     user = user.array();
-    var tryoutMember = client.users.find("id", user[0].id);
+    let tryoutMember = client.users.find("id", user[0].id);
 
     client.guilds.find("name", guildName).channels.find("name", "teamleaguechat").send("Welcome our newest Tryout to Born Gosu! " + mentionUser + " @here");
-    var roles = client.guilds.find("name", guildName).roles;
+    let roles = client.guilds.find("name", guildName).roles;
 
     tryoutMember.send(tryoutInfo);
 
-    var guildMember = client.guilds.find("name", guildName).member(tryoutMember);
+    let guildMember = client.guilds.find("name", guildName).member(tryoutMember);
     guildMember.addRole(roles.find("name", "Tryout Member").id);
     guildMember.addRole(roles.find("name", leagueString).id);
     guildMember.addRole(roles.find("name", raceString).id);
@@ -235,19 +237,18 @@ function tryout(channel, user, mentionUser, league, race) {
 
 function promote(channel, user, mentionUser){
     user = user.array();
-    var tryoutMember = client.users.find("id", user[0].id);
+    let tryoutMember = client.users.find("id", user[0].id);
 
     client.guilds.find("name", guildName).channels.find("name", "teamleaguechat").send("Welcome our newest Born Gosu member! " + mentionUser + " @here");
-    var roles = client.guilds.find("name", guildName).roles;
+    let roles = client.guilds.find("name", guildName).roles;
 
-    var guildMember = client.guilds.find("name", guildName).member(tryoutMember);
+    let guildMember = client.guilds.find("name", guildName).member(tryoutMember);
     guildMember.addRole(roles.find("name", "Born Gosu").id);
     guildMember.removeRole(roles.find("name", "Tryout Member").id);
 }
 
 function adminCheck(param) {
-    if(param) return true;
-    else return false;
+    return !!param;
 }
 
 function done(channel){
