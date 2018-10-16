6da88b3bb0f213d5ca357e5be5c2e0b24a07a871
AsheN <ashengale01@gmail.com>
2018-10-16 18:13:23 +0200

    ! improved quaterno's mention message

--- bot.js
+++ bot.js
@@ -845,7 +845,7 @@ client.on("message", (message) => {
             let Quaterno = client.users.find("id", "140143900886040576");
             if (message.isMentioned(Quaterno)) {
                 Quaterno.send("You got mentioned in this message!\n" + message.author.username + "  (" + message.createdAt + "): " + message.content);
-                message.channel.send("Quaterno is going on a walk for now! Your message has been relayed though. He wil be back Soon:tm:! (Sometime around December)");
+                message.channel.send("Quaterno is currently out for a walk! Your message has been relayed though. He wil be back Soon:tm: (Sometime around December)");
             }
         } catch (e) {}
     }
