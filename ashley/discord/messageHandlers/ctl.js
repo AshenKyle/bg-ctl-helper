a6c2b76e041012c9da0f0a888b7d38ec7b9b6ac9
Brandon Okert <bcokert@gmail.com>
2020-09-21 11:35:26 -0700

    Many upgrades to ctl manual mode

--- ashley/discord/messageHandlers/ctl.js
+++ ashley/discord/messageHandlers/ctl.js
@@ -86,7 +86,7 @@ const auto = async ({ discordInterface, discordMsg }) => {
   const msg = await discordMsg.channel.send(`Grabbing player and opponent information, and setting CTL Player role for ${discordMsg.mentions.members.map(m => m.displayName).join(", ")}. I'll delete this message when I'm done...`)
   const ctlPage = await htmlRequest('www.choboteamleague.com', '/home', e => {
     discordMsg.channel.send("Failed to fetch CTL site metadata")
-    console.log(`Error while fetching CTL site: ${e}`)
+    console.error(`Error while fetching CTL site: ${e}`)
   })
 
   if (!ctlPage.querySelectorAll('.article-content') || !ctlPage.querySelectorAll('.article-content')[0]) {
@@ -95,7 +95,8 @@ const auto = async ({ discordInterface, discordMsg }) => {
     return
   }
 
-  let [headerNode, bgNode]
+  let headerNode
+  let bgNode
   let isLeftBG
   let childNodes
   try {
@@ -204,6 +205,7 @@ ${setsAndPlayers[6].set} - ${setsAndPlayers[6].player}
 }
 
 const manual = async ({ discordInterface, discordMsg }) => {
+  const currentUserId = discordMsg.author.id
 
   await discordMsg.channel.send(`
   Alright. First step, go to <http://www.choboteamleague.com/home>, copy everything under the BornG area, then paste it here.
@@ -216,14 +218,31 @@ const manual = async ({ discordInterface, discordMsg }) => {
   Player6 | Player6#5678 vs. PlayerF | PlayerF#1234  [Deathaura LE]
   Player7 | Player7#5678 vs. PlayerG | PlayerG#1234  [Deathaura LE]\`\`\`
   I'll wait 60 seconds for you to paste it here :heart:
+  Also, I'll ignore everything that doesn't at least contain a \`vs.\`
   `)
 
-  const playerLines = await discordMsg.channel.awaitMessages(m => m.content.includes("vs."), { max: 1, time: 60000, errors: ["time"] })
+  const playerLines = await discordMsg.channel.awaitMessages(m => m.content.includes("vs.") && m.author.id === currentUserId, { max: 1, time: 60000, errors: ["time"] })
     .then(msgs => msgs.map(m => m.content)[0].split("\n").filter(l => l.includes("vs.")))
     .catch(() => discordMsg.channel.send("Hmm I didn't see anything matching after 60 seconds. I'll abort this `ctl manual`.") && false)
   if (!playerLines) return
   if (playerLines.length !== 7) {
     await discordMsg.channel.send(`Ummm... I was expecting 7 matching lines, but I found ${playerLines.length || 0}. Compare what you gave me to the example above and try again.`)
+    return
+  }
+
+  const sets = ["Platinum", "Platinum", "Diamond 2/3", "Diamond 2/3", "Diamond", "Diamond", "Masters 2/3"]
+  const errorMsg = playerLines.map((l, i) => {
+    if (l.trim().split(" ").length < 7) {
+      return `**Problem with set ${i+1} (${sets[i]})** - Something is missing. Here's yours with a correct example below it:\n\`${l}\`\n\`${"Player4 | Player4#5678 vs. PlayerD | PlayerD#1234  [Deathaura LE]"}\``
+    }
+  
+    if (!(l.includes("[") && l.includes("]"))) {
+      return `**Problem with set ${i+1} (${sets[i]})** - It looks like the map is not correct. Here's yours with a correct example below it:\n\`${l}\`\n\`${"Player4 | Player4#5678 vs. PlayerD | PlayerD#1234  [Deathaura LE]"}\``
+    }
+  }).filter(l => l).join("\n")
+
+  if (errorMsg !== "") {
+    await discordMsg.channel.send(`I found some issues with that entry:\n\n${errorMsg}\n\nCompare to the example above and try again :heart:`)
     return
   }
 
@@ -239,8 +258,8 @@ const manual = async ({ discordInterface, discordMsg }) => {
   pz\`\`\`
   I'll wait 120 seconds for you to enter it :alarm_clock:
   `)
-  const raceLines = await discordMsg.channel.awaitMessages(m => m.content.split("\n").length > 1, { max: 1, time: 120000, errors: ["time"] })
-    .then(msgs => msgs.map(m => m.content)[0].split("\n").map(l => l.toLowerCase()).filter(l => l.includes("z") || l.includes("t") || l.includes("p") || l.includes("r")))
+  const raceLines = await discordMsg.channel.awaitMessages(m => m.author.id === currentUserId && m.content.split("\n").length > 1, { max: 1, time: 120000, errors: ["time"] })
+    .then(msgs => msgs.map(m => m.content)[0].split("\n").map(l => l.trim().toLowerCase()).filter(l => l.includes("z") || l.includes("t") || l.includes("p") || l.includes("r")))
     .catch(() => discordMsg.channel.send("Hmm I didn't see anything matching after 120 seconds. I'll abort this `ctl manual`.") && false)
   if (!raceLines) return
   if (raceLines.length !== 7) {
@@ -249,7 +268,7 @@ const manual = async ({ discordInterface, discordMsg }) => {
   }
 
   await discordMsg.channel.send("Is Born Gosu on the left or the right? I'll wait 30 seconds.")
-  const leftOrRightLine = await discordMsg.channel.awaitMessages(m => m.content.length >= 1, { max: 1, time: 30000, errors: ["time"] })
+  const leftOrRightLine = await discordMsg.channel.awaitMessages(m => m.author.id === currentUserId && m.content.length >= 1, { max: 1, time: 30000, errors: ["time"] })
     .then(msgs => msgs.map(m => m.content)[0].toLowerCase())
     .catch(() => discordMsg.channel.send("Hmm I didn't see anything matching after 30 seconds. I'll abort this `ctl manual`.") && false)
   if (!leftOrRightLine) return
@@ -260,14 +279,13 @@ const manual = async ({ discordInterface, discordMsg }) => {
   const isLeftBG = leftOrRightLine[0] === "l"
 
   const ctlRoom = discordInterface.server.channels.find("name", "ctl")
-  const sets = ["Platinum", "Platinum", "Diamond 2/3", "Diamond 2/3", "Diamond", "Diamond", "Masters 2/3"]
-  const setsAndPlayers = [] // for display later on
-  await sets.forEach(async (set, i) => {
-    const lineSegments = playerLines[i].split(" ")
-    const bgName = isLeftBG ? lineSegments[1] : lineSegments[5]
-    const bgSc2Name = isLeftBG ? lineSegments[3] : lineSegments[7]
-    const oppName = isLeftBG ? lineSegments[5] : lineSegments[1]
-    const oppSc2Name = isLeftBG ? lineSegments[7] : lineSegments[3]
+  const summaryLines = []
+  const embedsToSend = sets.map((set, i) => {
+    const lineSegments = playerLines[i].trim().split(" ")
+    const bgName = isLeftBG ? lineSegments[0] : lineSegments[4]
+    const bgSc2Name = isLeftBG ? lineSegments[2] : lineSegments[6]
+    const oppName = isLeftBG ? lineSegments[4] : lineSegments[0]
+    const oppSc2Name = isLeftBG ? lineSegments[6] : lineSegments[2]
     const map = playerLines[i].split("[")[1].split("]")[0]
     const bgRaceIcon = isLeftBG ? letterToRaceIcon[raceLines[i][0]] : letterToRaceIcon[raceLines[i][1]]
     const oppRaceIcon = isLeftBG ? letterToRaceIcon[raceLines[i][1]] : letterToRaceIcon[raceLines[i][0]]
@@ -288,9 +306,9 @@ const manual = async ({ discordInterface, discordMsg }) => {
     }
     opponent.replays = `https://sc2replaystats.com/ladder/search?type=1v1&player=${opponent.name}`
 
-    setsAndPlayers.push({player: player.name, set }) // used for display afterwards
+    summaryLines.push(`\`${set}: ${player.name} (${player.sc2name}) vs ${opponent.name} (${opponent.sc2name}) on ${map}\``)
 
-    await ctlRoom.send({
+    return {
       embed: {
         title: `${set} Set`,
         fields: [{
@@ -315,7 +333,31 @@ const manual = async ({ discordInterface, discordMsg }) => {
           inline: false,
         }],
       },
-    })
+    }
+  })
+
+  await discordMsg.channel.send(`Here's what I got\n${summaryLines.join("\n")}\n\nType \`yes\` to confirm. I'll wait 120 seconds.`)
+  const isConfirmed = await discordMsg.channel.awaitMessages(m => m.author.id === currentUserId && m.content.trim().length > 0, { max: 1, time: 120000, errors: ["time"] })
+    .then(msgs => msgs.map(m => m.content)[0].toLowerCase().trim())
+    .catch(() => false)
+  if (!isConfirmed) {
+    await discordMsg.channel.send("Hmm I didn't see anything after 120 seconds. I'll abort this `ctl manual`.")
+    return
+  }
+  if (isConfirmed != "yes") {
+    await discordMsg.channel.send("I didn't get a `yes`, so I'll abort this `ctl manual`")
+    return
+  }
+
+  await discordMsg.channel.send(`Got it. It'll take me a few seconds to send everything...`)
+
+  await embedsToSend.forEach(async (embed, i) => {
+    try {
+      await ctlRoom.send(embed)
+    } catch (e) {
+      discordMsg.channel.send(`Something went wrong while I was sending the data to Discord for set ${i+1} (${sets[i]}). **You'll need to run \`+ctl manual to clean up what has already worked so far and then try again**.\``)
+      console.error(`Error during sending embeds from Ctl Manual: ${e}`)
+    }
   })
 
   const ctlRoleID = discordInterface.server.roles.find("name", CTLPLAYERS).id
@@ -340,13 +382,5 @@ This is a private channel for ctl players to schedule and plan for their matches
 **Report the outcome:** DM your replay to the CTL captain, but keep the result private in case we cast your games.
   `)
 
-  await ctlRoom.setTopic(`
-${setsAndPlayers[0].set} - ${setsAndPlayers[0].player}
-${setsAndPlayers[1].set} - ${setsAndPlayers[1].player}
-${setsAndPlayers[2].set} - ${setsAndPlayers[2].player}
-${setsAndPlayers[3].set} - ${setsAndPlayers[3].player}
-${setsAndPlayers[4].set} - ${setsAndPlayers[4].player}
-${setsAndPlayers[5].set} - ${setsAndPlayers[5].player}
-${setsAndPlayers[6].set} - ${setsAndPlayers[6].player}
-  `)
+  await discordMsg.channel.send(`All done!`)
 }
